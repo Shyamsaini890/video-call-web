707de48a5b55a9e5f7fc95ee8c431391f667ce12
Shyamsaini890 <Shyamsundersaini71@gmail.com>
2023-09-28 20:05:04 +0530

    Changed websocket address

--- src/context/SocketProvider.js
+++ src/context/SocketProvider.js
@@ -10,7 +10,13 @@ export const useSocket = () => {
 };
 
 export const SocketProvider = (props) => {
-  const socket = useMemo(() => io("https://server-three-liard.vercel.app/"), []);
+  const socket = useMemo(() => io("wss://shyam001.glitch.me/"), []);
+
+  socket.on("connect_error", (error) => {
+    console.error("Socket connection error:", error);
+    // You can handle the error here, e.g., show an error message to the user.
+  });
+
   return (
     <SocketContext.Provider value={socket}>
       {props.children}
